7f45fe2a94f8c2a4408477b0eee259b43ac44000
Oriol65 <150459853+Oriol65@users.noreply.github.com>
2025-10-10 20:48:46 +0100

    Completed removeFromArray and passed tests

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let filterArray=[]
+    for (let i=1;i<=arguments.length;i++){
+        filterArray.push(arguments[i]);
+    }
+    let result = arr.filter((item) => !filterArray.includes(item))
+    return (result);
 };
 
 // Do not edit below this line
