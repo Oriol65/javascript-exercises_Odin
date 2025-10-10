36e2bda623a5d4ba0257c4e0d06ef763baec6ec4
Oriol65 <150459853+Oriol65@users.noreply.github.com>
2025-10-10 20:28:21 +0100

    Completed reverseString and passed all tests

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr=string.split('');
+    let invertedArray=[];
+    for (let i=arr.length;i>=0;i--){
+        invertedArray.push(arr[i])
+    }
+    return(invertedArray.join(''))
 };
 
 // Do not edit below this line
