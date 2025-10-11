db59835d18357c079e6a9e854b79f46e6186c241
Oriol65 <150459853+Oriol65@users.noreply.github.com>
2025-10-11 18:02:50 +0100

    completed sumAll and passed all tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let result = 0;
+    if (a<0 || b<0 || !Number.isInteger(a+b)){
+        return('ERROR');
+    } else if (a>b){
 
+        return (sumElements(b,a));
+    } else {
+
+        return (sumElements(a,b));
+    }
 };
-
+function sumElements (min,max){
+    let result=0;
+    for (let i=min; i<=max; i++){
+        result=result+i;
+    }
+    return (result)
+}
 // Do not edit below this line
 module.exports = sumAll;
