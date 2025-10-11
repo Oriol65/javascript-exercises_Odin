98a6f588fd7d8c1c5f80290d6adfaf42201747cf
Oriol65 <150459853+Oriol65@users.noreply.github.com>
2025-10-11 18:30:55 +0100

    Completed tempConversion and passed all tests

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let tempC=(num-32)*5/9;
+  return (Math.round(tempC*10)/10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let tempF=(num*9/5)+32;
+  return(Math.round(tempF*10)/10);
 };
 
 // Do not edit below this line
