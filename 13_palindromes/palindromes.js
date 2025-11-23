f26fbe212c5f2451b70921157dfab60bac22460f
Oriol65 <150459853+Oriol65@users.noreply.github.com>
2025-11-23 09:56:03 +0000

    Finished Excercise 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
-
+const phrase = function (test){
+    const lower = test.toLowerCase();
+    console.log(lower)
+    const replaced = lower.replace(/[,./<>?;':"!@& ]/g,'')
+    console.log(replaced)
+    return(replaced);
+}
+const palindromes = function (test) {
+    const ptest = phrase(test);
+    const len =ptest.length;
+    console.log(len/2)
+    for (let i=0; i<= len/2; i++){
+        const j= len-1-i;
+        if (ptest[i] != ptest[j]){
+            return(false)  
+        }
+    }
+    return(true)
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
