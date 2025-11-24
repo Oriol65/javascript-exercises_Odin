76f4854626ed3eb8bcab34962ffb45bf4834d130
Oriol65 <150459853+Oriol65@users.noreply.github.com>
2025-11-24 22:02:08 +0000

    Exercise 16 completed and tested

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+function determineOldest(a,b){
+    //Assume that everyone is alive
+    const aBirth = a.yearOfBirth;
+    const bBirth = b.yearOfBirth;
+    let aDeath = 2025;
+    let bDeath = 2025;
+    // if there is a year of death change it to that
+    if(a.yearOfDeath){
+        aDeath= a.yearOfDeath
+    }
+    if (b.yearOfDeath){
+        bDeath=b.yearOfDeath
+    }
+    //find out age and return
+    let aAge=aDeath-aBirth
+    let bAge = bDeath-bBirth
+    if (aAge>bAge){
+        return(-1)
+    } else {
+        return(1)
+    }
+}
 
+const findTheOldest = function(obj) {
+
+    oldest = obj.sort((a,b) => determineOldest(a,b))
+    return(oldest[0])
 };
 
 // Do not edit below this line
