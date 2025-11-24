cc688ac9271eeae4ddcb5d6a1f0bd8cd1d0f8bd3
Oriol65 <150459853+Oriol65@users.noreply.github.com>
2025-11-24 21:19:22 +0000

    Fibonacci Exercise completed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    const number = Number(input)
+    if (number<0){
+        return('OOPS')
+    }
+    if (number == 0){
+        return(0)
+    }
+ 
+    let total = 1;
+    let past = 0;
 
+    for(let i=0; i< number-1; i++) {
+        let middle = total;
+        total = total + past;
+        past = middle;
+    }
+    return (total)
 };
 
 // Do not edit below this line
